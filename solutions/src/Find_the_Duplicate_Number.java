a4e90894b8ce1c268d269c7a3542839c227b36b6
Akshit <akshittayal24@gmail.com>
2024-07-14 13:50:56 +0530

    Solve Find the Duplicate Number

---
+++ solutions/src/Find_the_Duplicate_Number.java
@@ -0,0 +1,22 @@
+public class Find_the_Duplicate_Number {
+    public int findDuplicate(int[] nums) {
+        int slow=0, fast=0;
+        int check = 0;
+        while( true ){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if( slow == fast ){
+                break;
+            }
+        }
+        while( true ){
+            slow = nums[slow];
+            check = nums[check];
+            if( slow == check ){
+                break;
+            }
+        }
+
+        return check;
+    }
+}
