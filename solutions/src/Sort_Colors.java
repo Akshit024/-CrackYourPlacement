a11ec6b26816b0242e921cdb01c6eb8a4668668d
Akshit <akshittayal24@gmail.com>
2024-07-14 14:01:34 +0530

    Solve Sort Colors

---
+++ solutions/src/Sort_Colors.java
@@ -0,0 +1,23 @@
+public class Sort_Colors {
+    public static void swap(int[] nums,int i,int j){
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void sortColors(int[] nums) {
+        int l=0;
+        int m=0;
+        int r=nums.length-1;
+
+        while(m<=r){
+            if(nums[m] == 0 ){
+                swap(nums,m++,l++);
+            }else if(nums[m]==1){
+                m++;
+            }else{
+                swap(nums,m,r--);
+            }
+        }
+
+    }
+}
